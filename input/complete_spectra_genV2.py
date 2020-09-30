c33115f7efb2d63aa3e82137481fecf92c84f75a
GwydionJon <35529247+GwydionJon@users.noreply.github.com>
2020-09-30 15:30:03 +0200

    Update complete_spectra_genV2.py

--- input/complete_spectra_genV2.py
+++ input/complete_spectra_genV2.py
@@ -252,7 +252,7 @@ def setup_dir_structure(path_dict):
 #print(get_input_data())
 #change to current path
 #current_path=(os.path.dirname(__file__))
-#print(current_path)
+print(current_path)
 filename = sys.argv[0]
 current_path=(os.path.dirname(filename))
 
