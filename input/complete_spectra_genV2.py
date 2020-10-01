3939797afab6a87f057bd11e06521023f1f2bd84
GwydionJon <35529247+GwydionJon@users.noreply.github.com>
2020-10-01 11:06:13 +0200

    Update complete_spectra_genV2.py

--- input/complete_spectra_genV2.py
+++ input/complete_spectra_genV2.py
@@ -151,10 +151,11 @@ def commit_jobs(path_dict, no_of_submits):
 	current_path=os.getcwd()
 
 	#change directory to have output in the right place
-	os.chdir(path_dict["output"])
 
 	print("current",current_path)
 	if(len(all_input_data)!=0):
+		os.chdir(path_dict["output"])
+
 		print("Total of ", len(all_input_data), " jobs remaining.")
 		if(len(all_input_data)<=no_of_submits):
 			no_of_submits=len(all_input_data)
